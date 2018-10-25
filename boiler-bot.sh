f61e0e328216557dfc93a3d4c31984188c33bec9
phil1220 <philemon@infabytes.com>
2018-10-25 17:51:31 +0530

    Few modifications for downloading from s3

--- boiler-bot.sh
+++ boiler-bot.sh
@@ -26,28 +26,26 @@ then
 	#Download and save ROOT-CA to /greengrass/certs/
 	sudo wget -O /greengrass/certs/root.ca.pem  http://www.symantec.com/content/en/us/enterprise/verisign/roots/VeriSign-Class%203-Public-Primary-Certification-Authority-G5.pem
 
-	#Download cert keys from S3 bucket using CURL to /greengrass/certs/
-	s3FilePath="boilerbot/devices-certificates/1539963012-03d7"
-	amzFile="1539963012-03d7."
-	declare -a certExt=("key" "public.key")		## declare an array with values as certificate extensions
-	bucket="davidf-provisioning-test"
-	resource="/${bucket}/${amzFile}/${s3FilePath}"
-	contentType="device/certificate"
-	dateValue=`date -R`
-	stringToSign="GET\n\n${contentType}\n${dateValue}\n${resource}"
-	s3Key="AKIAJ2YOT47HVNNQYIHA"
-	s3Secret="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"
-	signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
-
 	for i in "${certExt[@]}"
 	do
-		sudo curl  -H "Host: ${bucket}.s3.amazonaws.com" \
+		file="boilerbot/devices-certificates/c48043bb-c0c5-4b44-a5fa-6dac3eff5d78/c48043bb-c0c5-4b44-a5fa-6dac3eff5d78."
+		bucket="boilerbot-provisioning"
+		resource="/${bucket}/${file}$i"
+		contentType="device/certificate"
+		dateValue=`date -R`
+		stringToSign="GET\n\n${contentType}\n${dateValue}\n${resource}"
+		s3Key="AKIAI56LNSUKIXBZWPMQ"
+		s3Secret="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"
+		signature=`echo -en ${stringToSign} | openssl sha1 -hmac ${s3Secret} -binary | base64`
+		echo $resource
+
+		curl  -H "Host: ${bucket}.s3.amazonaws.com" \
 		     -H "Date: ${dateValue}" \
 		     -H "Content-Type: ${contentType}" \
 		     -H "Authorization: AWS ${s3Key}:${signature}" \
-		     https://${bucket}.s3.amazonaws.com/${s3FilePath}${amzFile}$i -o "/greengrass/certs/${amzFile}$i"
+		     --fail https://${bucket}.s3.amazonaws.com/${file}$i -o "/greengrass/certs/${file}$i"
 	done
 fi
 
 # Starting greengrassd
-sudo bash /greengrass/ggc/core/greengrassd start
+sudo /greengrass/ggc/core/greengrassd start
