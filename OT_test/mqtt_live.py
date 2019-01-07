4a8b513a7e5a295f8a0db28522738e8ecdae2fd5
bala_infabytes <bala@infabytes.com>
2019-01-07 20:28:58 +0000

    added for open therm

--- OT_test/mqtt_live.py
+++ OT_test/mqtt_live.py
@@ -8,10 +8,20 @@ import subprocess
 import shlex
 import re
 import os
-import serial
+from datetime import datetime
+from threading import Timer
+
+d=datetime.today()
+#e=d.replace(day=d.day+1, hour=1, minute=0, second=0, microsecond=0)
+#e=d.replace(day=d.day+1, hour=1, minute=0, second=0, microsecond=0)
+#delta_t=e-d
+
+#secs=delta_t.seconds+1
+secs=3
 
 devID=os.environ['dev_id']
 send=False
+live=0
 
 MQTT_PORT = 8883
 MQTT_KEEPALIVE_INTERVAL = 45
@@ -31,15 +41,15 @@ def on_publish(client, userdata, mid):
 
 # Define on_message event function
 def on_message(client, userdata, msg):
-    print("topic: "+msg.topic)
-    print("payload: "+str(msg.payload))
-    data = json.loads(str(msg.payload))
-    global send, clkStart
-    if data['devID'] == devID:
-		print("Device ID Match!!")
+        print("topic: "+msg.topic)
+        print("payload: "+str(msg.payload))
+        data = json.loads(str(msg.payload))
+        global send, clkStart
+        if data['devID'] == devID:
+        	print("Device ID Match!!")
 		send = True
 		clkStart = time.time()
-    else:
+        else:
 		send = False
 
 # Initiate MQTT Client
@@ -59,13 +69,8 @@ client.subscribe('testin/'+devID , 1 )
 client.loop_start()
 time.sleep(2)
 
-
-ser = serialport = serial.Serial("/dev/ttyACM0", 9600, timeout=1)
-
-
-
 # List of commands from boiler
-cmd_list =     	['DHWtemperature',
+cmd_list =      ['DHWtemperature',
                 'returntemp',
                 'flowrate',
                 'CHwaterpressure',
@@ -74,69 +79,91 @@ cmd_list =     	['DHWtemperature',
                 'Boilerdiagcode',
                 'OEMdiagcode']
 
+ems_json =      {"OEMdiagcode":"op7",
+                "Boilerflowtemp":"op2",
+                "returntemp":"op4",
+                "CHwaterpressure":"op5",
+                "OPFlags":"op1",
+                "Relativemodlevel":"op3",
+                "DHWtemperature":"op10",
+                "flowrate":"op9",
+                "appflags":"op6",
+				"oemfault":"op8"}
+
 # Declaring list of datas to be stored and dictionary for indexing
-arr_data = []
+arr_data = [len(cmd_list)] * 0
 data_dict = {}
-batchVal = []	# List containing all 4 second data
-data = {}		# Store data for the JSON payload
+batchVal = []   # List containing all 4 second data
+data = {}       # Store data for the JSON payload
 
 # Loop to get the boiler parameters and temporarily store in a list before storing in a dictionary
 rec_count = 0
 n = 0
 while True:
 
-	if (ser.isOpen == False):
-		ser.open()
-	response = serialport.readlines()
-	print "serialdata"
-	response = str(response)
-	print response
+        if (ser.isOpen == False):
+                ser.open()
+        response = serialport.readlines()
+        print "serialdata"
+        #response = str(response)
+        response = [i.replace("\r\n","") for i in response]
+        print response
 
+        n = 0
+        #for y in response:
+        for x in cmd_list:
+                xx = x+"=.*"
+                for i in response:
+                   ss=re.search(xx,i,flags=0)
+                   if (ss):
+                        val=ss.group(0)
+                        #print val
+                        final_result = re.split('=',val)
+                        #print final_result[1]
+                        #data_dict['recutc'] = int(time.time())
+                        #arr_data[y] = float(final_result[1])
+                        data_dict[ems_json[x]] = float(final_result[1])
+                   n = n+1
 
-	#for y in response:
-	for x in cmd_list:
-		xx = x+"=........"
-		#if x in response:
-		#	print(response[response.find(x):11])
-		ss=re.search(xx,response,flags=0)
-		#print ss
-		#response.find(j)  # z=re.search(j,response)
-		if (ss):
-			val=ss.group(0)
-			#print val
-			final_result = re.split('=',val)
-			#print final_result[1]
-			data_dict[x] = float(final_result[1])
-			print data_dict
+        #for j,i in enumerate(cmd_list):
+            #print data_dict[i
+            #print j
+            #data_dict[i] = float(arr_data[j])
+            #print i
 
-	batchVal.append(data_dict)
-	time.sleep(4)
-	rec_count += 1
+        #batchVal.append(data_dict)
+        data_dict['timestamp'] = int(time.time())
+        print data_dict
+        time.sleep(4)
+        rec_count += 1
+        batchVal.append(data_dict)
 
-	# Send live data into "'testout/'+devID" on demand triggered on
-	# message with devID same as the devicedID in "'testout/'+devID"
-	if send :
-		if (time.time() - clkStart) < 300:
-			live_payload = json.dumps(data_dict)
-			client.publish('test/batchData', live_payload, qos=1)
-			print("Sending live data!!")
-		else:
-			send = False
-			clkStart = 0
-	data_dict = {} # Flush dictionary before storing the next set of 4 sec data
+        # Send live data into "'testout/'+devID" on demand triggered on
+        # message with devID same as the devicedID in "'testout/'+devID"
+        if send :
+                if (time.time() - clkStart) < 300:
+                        live_payload = json.dumps(data_dict)
+                        client.publish('test/liveData', live_payload, qos=1)
+                        print("Sending live data!!")
+                else:
+                        send = False
+                        clkStart = 0
+        data_dict = {} # Flush dictionary before storing the next set of 4 sec data
 
-	# Batch the 4 second data to 5 minute intervals
-	if rec_count == 75:
-		data['date'] = time.strftime("%x")
-		data['time'] = time.strftime("%X")
-		data['data'] = batchVal
-                data['devID'] = devID
-		payload = json.dumps(data)
-		client.publish('test/batchData', payload, qos=1)	# Send The batched data to topic "batch/test"
-		print("Sending Batch data!!")
+        # Batch the 4 second data to 5 minute intervals
+        if rec_count == 75:
+                #data['date'] = time.strftime("%x")
+                #data['time'] = time.strftime("%X")
+				data['type']="data"
+        		data['deviceid']=devID
+        		data['bus']="OT1.0"
+        		data['data'] = batchVal
+                payload = json.dumps(data)
+                client.publish('test/batchData', payload, qos=1)        # Send The batched data to topic "batch/test"
+                print("Sending Batch data!!")
 
-		# Clearing the list and dictionaries (arr_data is cleared at the start of the while loop)
-		batchVal[:] = []
-		data_dict.clear()
-		data.clear()
-		rec_count = 0
+                # Clearing the list and dictionaries (arr_data is cleared at the start of the while loop)
+                batchVal[:] = []
+                data_dict.clear()
+                data.clear()
+                rec_count = 0
--- test/mqtt_live.py
+++ test/mqtt_live.py
@@ -8,9 +8,20 @@ import subprocess
 import shlex
 import re
 import os
+from datetime import datetime
+from threading import Timer
+
+d=datetime.today()
+#e=d.replace(day=d.day+1, hour=1, minute=0, second=0, microsecond=0)
+#e=d.replace(day=d.day+1, hour=1, minute=0, second=0, microsecond=0)
+#delta_t=e-d
+
+#secs=delta_t.seconds+1
+secs=3
 
 devID=os.environ['dev_id']
 send=False
+live=0
 
 MQTT_PORT = 8883
 MQTT_KEEPALIVE_INTERVAL = 45
@@ -30,15 +41,15 @@ def on_publish(client, userdata, mid):
 
 # Define on_message event function
 def on_message(client, userdata, msg):
-    print("topic: "+msg.topic)
-    print("payload: "+str(msg.payload))
-    data = json.loads(str(msg.payload))
-    global send, clkStart
-    if data['devID'] == devID:
-		print("Device ID Match!!")
+        print("topic: "+msg.topic)
+        print("payload: "+str(msg.payload))
+        data = json.loads(str(msg.payload))
+        global send, clkStart
+        if data['devID'] == devID:
+        	print("Device ID Match!!")
 		send = True
 		clkStart = time.time()
-    else:
+        else:
 		send = False
 
 # Initiate MQTT Client
@@ -58,21 +69,46 @@ client.subscribe('testin/'+devID , 1 )
 client.loop_start()
 time.sleep(2)
 
+spec_list= [["currenterror","eb1"],
+            ["Statenumber","eb2"],
+            ["flowtemp","eb3"],
+            ["ReturnTemp","eb4"],
+            ["IonisationVoltageLevel","eb5"],
+            ["fanspeed","eb6"],
+            ["waterpressure","eb7"],
+            ["HwcWaterflow","eb8"],
+            ["HwcTemp","eb9"],
+            ["PositionValveSet","eb10"],
+            ["Flame","eb11"],
+            ["HwcDemand","eb12"],
+            ["ACRoomthermostat","eb13"],
+            ["DCRoomthermostat","eb14"]
+                ]
 # List of commands from boiler
 cmd_list =     	["flowtemp",
                 "waterpressure",
                 "IonisationVoltageLevel",
                 "fanspeed",
-                "ExternGasvalve",
                 "Statenumber",
                 "ReturnTemp",
                 "PumpHwcFlowSum",
+                "HwcWaterflow",
                 "HwcTemp",
-                "HcHours",      ##Diagnostic parameters##
-                "HwcHours",
-                "CounterStartattempts1",
-                "CounterStartattempts2",
-                "currenterror"]
+                "PositionValveSet",
+                "HwcTempDesired",
+                "flame",
+                "currenterror",
+                "ACRoomthermostat",
+                "DCRoomthermostat"]
+
+# List of maintanence data from boiler
+m_cmd_list =    [["HwcHours","eb16"],
+                ["HcHours","eb19"],
+                ["CounterStartattempts1","eb17"],
+                ["CounterStartattempts2","eb18"],
+                ["averageIgnitiontime","eb21"],
+                ["HwcSetPotmeter","eb22"],
+                ["flowtempdesired","eb24"]]
 
 # Declaring list of datas to be stored and dictionary for indexing
 arr_data = []
@@ -80,12 +116,44 @@ data_dict = {}
 batchVal = []	# List containing all 4 second data
 data = {}		# Store data for the JSON payload
 
-# Loop to get the boiler parameters and temporarily store in a list before storing in a dictionary
+def maint_data():
+    arr_data[:] = []
+    data_dict.clear()
+    data_dict['recutc'] = int(time.time())
+    data_dict['type']="maint"
+    data_dict['deviceid']=devID
+    data_dict['bus']="ebus2.1"
+    for i in m_cmd_list:
+        cmd_arr = "ebusctl r -m 10 {}".format(i[0])
+        args =  shlex.split(cmd_arr)
+        cmd_output = subprocess.Popen(args,stdout=subprocess.PIPE)
+        result = cmd_output.communicate()
+        temp_result = result[0]
+        #print "temp_result="+str(temp_result)
+        final_result = re.split('; |,|\:|\n|\;',temp_result)
+        #print "final_result="+str(final_result)
+        arr_data.append(final_result[0])
+        #print "arr_data="+str(arr_data)
+        # Loop to store the boiler parameters in a dictionary opposite to the parameter names
+        n = 0
+    for j in m_cmd_list:
+        if arr_data[n] == '-':
+            arr_data[n] = str(0)
+        if arr_data[n] == 'ERR':
+            arr_data[n] = -1
+        data_dict[j[1]] = float(arr_data[n])
+        n = n+1
+    payload = json.dumps(data_dict)
+    client.publish('test/maintData', payload, qos=1) #send maintanence data
+    print("Maintanance data sent")
+    print str(payload)
+    data_dict.clear()
+
 rec_count = 0
 while True:
 	arr_data[:] = []
-	for i in cmd_list:
-		cmd_arr = "ebusctl r -m 10 {}".format(i)
+	for i in spec_list:
+		cmd_arr = "ebusctl r -m 10 {}".format(i[0])
 		args =  shlex.split(cmd_arr)
 		cmd_output = subprocess.Popen(args,stdout=subprocess.PIPE)
 		result = cmd_output.communicate()
@@ -93,24 +161,36 @@ while True:
 		final_result = re.split('; |,|\:|\n|\;',temp_result)
 		arr_data.append(final_result[0])
 
+	#data_dict['timestamp'] = int(time.time())
 	data_dict['recutc'] = int(time.time())
-	data_dict['recdate'] = time.strftime("%x")
-	data_dict['rectime'] = time.strftime("%X")
-        data_dict['devID'] = devID
+        #data_dict['recdate'] = time.strftime("%x")
+	#data_dict['rectime'] = time.strftime("%X")
+        #data_dict['devID'] = devID
 
 	# Loop to store the boiler parameters in a dictionary opposite to the parameter names
 	n = 0
-	for j in cmd_list:
-		if j == "currenterror":
+	for j in spec_list:
+		if j[0] == "currenterror":
 			if  arr_data[n] == '-':
-				data_dict[j] = str(0)
+				data_dict[j[1]] = int(0)
+                                live = 0
 			else:
-				data_dict[j] = str('F'+arr_data[n])
+				data_dict[j[1]] = str(arr_data[n])
+                                live = live+1
 		else:
-			data_dict[j] = float(arr_data[n])
+                        if arr_data[n] == 'on' or arr_data[n] == 'yes':
+                            arr_data[n] = 1
+                        if arr_data[n] == 'off' or arr_data[n] == 'no':
+                            arr_data[n] = 0
+                        if arr_data[n] == '-':
+                            arr_data[n]=0
+		        if arr_data[n] == 'ERR':
+                            arr_data[n] = -1
+                        data_dict[j[1]] = float(arr_data[n])
 		n = n+1
 
 	batchVal.append(data_dict)
+        print str(data_dict)
 	time.sleep(4)
 	rec_count += 1
 
@@ -118,8 +198,12 @@ while True:
 	# message with devID same as the devicedID in "'testout/'+devID"
 	if send :
 		if (time.time() - clkStart) < 300:
-			live_payload = json.dumps(data_dict)
-			client.publish('batch/testlive', live_payload, qos=1)
+                        data['type']="dataLive"
+                        data['deviceid']=devID
+                        data['bus']="ebus2.1"
+			data['data'] = data_dict
+                        live_payload = json.dumps(data)
+			client.publish('test/batchlive', live_payload, qos=1)
 			print("Sending live data!!")
 		else:
 			send = False
@@ -127,12 +211,16 @@ while True:
 	data_dict = {} # Flush dictionary before storing the next set of 4 sec data
 
 	# Batch the 4 second data to 5 minute intervals
-	if rec_count == 75:
-		data['date'] = time.strftime("%x")
-		data['time'] = time.strftime("%X")
-		data['data'] = batchVal
+	if rec_count == 3 or live == 1:
+                data.clear()
+		#data['date'] = time.strftime("%x")
+		#data['time'] = time.strftime("%X")
+		data['type']="data"
+        data['deviceid']=devID
+        data['bus']="ebus2.1"
+        data['data'] = batchVal
 		payload = json.dumps(data)
-		client.publish('batch/test', payload, qos=1)	# Send The batched data to topic "batch/test"
+		client.publish('test/batchData', payload, qos=1)	# Send The batched data to topic "batch/test"
 		print("Sending Batch data!!")
 
 		# Clearing the list and dictionaries (arr_data is cleared at the start of the while loop)
@@ -140,4 +228,10 @@ while True:
 		data_dict.clear()
 		data.clear()
 		rec_count = 0
+                maint_data()
 
+            #Maintenance data start
+
+
+#t = Timer(secs, maint_data)
+#t.start()
